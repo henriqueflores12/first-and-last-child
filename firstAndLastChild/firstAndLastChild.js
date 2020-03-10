06457fe75ca8a00d0d386f331a5f35fb3b8a8fc2
fagner henrique flores <henriquefagner@hotmail.com>
2020-03-10 18:38:53 -0500

    final commit

---
+++ firstAndLastChild/firstAndLastChild.js
@@ -0,0 +1,10 @@
+// //select the starting point and find its children
+// var startItem = document.getElementsByTagName('li')[0];
+// var firstItem = startItem.firstChild;
+// var lastItem = startItem.lastChild;
+
+// //change the values of the children's class attributes
+// firstItem.setAttribute("class, complete");
+// lastItem.setAttribute("class, cool");
+// console.log(document);
+//not working right i need to fix it...
